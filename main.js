efc8f585726e999ec7923fca4d7725267b7887a7
missygp29 <125516970+missygp29@users.noreply.github.com>
2023-03-14 22:00:41 -0400

    March 14 night
    Updates to the JS for parsing and clue pages

--- main.js
+++ main.js
@@ -1,20 +1,50 @@
-// this empty JavaScript file can be filled with code later
-// now it is just two comment examples in JavaScript
 
-//This is the input and button part from the parsing page. need to repeat it with different IDs below for the clues page.
+//These are the inputs for the parsing page. Need to fix the thing.
 
-const btn = document.getElementById("button")
-const input = document.getElementById('clue1')
-const label = document.getElementById('label1')
-  btn.addEventListener("click", function() {
-    if (document.getElementById("clue1").value === "zephyr" || document.getElementById("clue1").value === "Zephyr") {
+const btnParsing1= document.getElementById("button1-parsing")
+const inputParsing1 = document.getElementById('clue1-parsing')
+const labelParsing1 = document.getElementById('label-parsing')
+  btnParsing1.addEventListener("click", function() {
+    if (document.getElementById("clue1-parsing").value === "pish" || document.getElementById("clue1-parsing").value === "Pish") {
       alert("That's right - nice job!") 
-      label.innerHTML = 'Now try another one below!'
-      input.value = '';
+      labelParsing1.innerHTML = 'Now try another one below!'
+      inputParsing1.value = '';
     } else {
       alert("Not quite; let's try again")
-      label.innerHTML = 'let\'s think about this again'
-      input.value = '';
+      labelParsing1.innerHTML = 'let\'s try again'
+      inputParsing1.value = '';
+    }
+  })
+
+//These are the inputs for the Clues page.
+
+const btn1 = document.getElementById("button1")
+const input1 = document.getElementById('clue1')
+const label1 = document.getElementById('label1')
+  btn1.addEventListener("click", function() {
+    if (document.getElementById("clue1").value === "pish" || document.getElementById("clue1").value === "Pish") {
+      alert("That's right - nice job!") 
+      label1.innerHTML = 'Now try another one below!'
+      input1.value = '';
+    } else {
+      alert("Not quite; let's try again")
+      label1.innerHTML = 'let\'s try again'
+      input1.value = '';
+    }
+  })
+
+  const btn2 = document.getElementById("button2")
+const input2 = document.getElementById('clue2')
+const label2 = document.getElementById('label2')
+  btn2.addEventListener("click", function() {
+    if (document.getElementById("clue1").value === "oftentimes" || document.getElementById("clue2").value === "Oftentimes") {
+      alert("That's right - nice job!") 
+      label2.innerHTML = 'Now try another one below!'
+      input2.value = '';
+    } else {
+      alert("Not quite; let's try again")
+      label2.innerHTML = 'let\'s try again'
+      input2.value = '';
     }
   })
 
